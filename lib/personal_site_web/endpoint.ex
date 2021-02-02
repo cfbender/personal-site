6a295a589375bdc65973f8cd04f40f0449f8be4e
Cody Bender <cody.bender@pdq.com>
2021-02-02 13:28:28 -0700

    fix: favicon deployment

--- lib/personal_site_web/endpoint.ex
+++ lib/personal_site_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule PersonalSiteWeb.Endpoint do
     at: "/",
     from: :personal_site,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
