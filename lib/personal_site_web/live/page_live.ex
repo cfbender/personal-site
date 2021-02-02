c27f8f4ba797f9ce47d3898de6d806d41a6e4205
Cody Bender <cody.bender@pdq.com>
2021-02-02 12:58:20 -0700

    feat: dark mode

--- lib/personal_site_web/live/page_live.ex
+++ lib/personal_site_web/live/page_live.ex
@@ -3,37 +3,23 @@ defmodule PersonalSiteWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    {:ok, assign(socket, dark: true)}
   end
 
   @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+  def handle_event("toggle_dark", data, socket) do
+    value = data |> Map.get("value") |> convert_dark()
+
+    {:noreply, assign(socket, :dark, value)}
   end
 
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
+  defp convert_dark(val) do
+    case val do
+      "true" ->
+        true
 
       _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
+        false
     end
-  end
-
-  defp search(query) do
-    if not PersonalSiteWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
   end
 end
